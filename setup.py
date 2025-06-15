80fe30e61dc74e959206ae6ced1101547a8e36d7
samson721 <samyjadhav2001@gmail.com>
2025-06-15 16:16:36 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Samson Jadhav",
+    author_email="samyjadhav2001@gmail.com",
+    packages=find_packages(),
+    install_requires = [],
+)
